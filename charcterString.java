1a4e16b93e2967050c4215ac260b361bf82dcb29
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-05-31 21:35:49 +0530

    Add files via upload

---
+++ charcterString.java
@@ -0,0 +1,12 @@
+package javaInterviewProgram;
+
+public class charcterString 
+{
+	public static void main(String []args)
+	{
+		char[] ch={'j','a','v','a','t','p','o','i','n','t'};  
+		String s=new String(ch);  
+		System.out.println(s);
+	}
+
+}
