d84f997657164781dfb104b37ac57510b206d213
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-05-31 21:34:01 +0530

    Add files via upload

---
+++ Arraysbasic.java
@@ -0,0 +1,36 @@
+package javaInterviewProgram;
+
+public class Arraysbasic 
+{
+	public static void main(String []rgs)
+	{
+		int []arr=new int[5];
+		arr[0]=1;
+		arr[1]=2;
+		arr[2]=3;
+		arr[3]=4;
+		arr[4]=5;
+		
+		for(int i=0; i<=arr.length-1; i++)
+		{
+			System.out.print(arr[i]+",");
+		}
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+	}
+
+}
