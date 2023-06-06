2a77611c1fd100357aadf45d7bf9f2383629a811
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-06-06 15:53:12 +0530

    Add files via upload

--- Hello.java
+++ Hello.java
@@ -1,10 +1,10 @@
-package OctoberProgram;
+package target200qQQQTTOOnnSS;
 
 public class Hello 
 {
 	public static void main(String []args)
 	{
-		System.out.println("vishal Rai frist Program");
+		System.out.println("vishal rai");
 	}
 
 }
