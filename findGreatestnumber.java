96d6ebf6c4cd19a796aa4e0f147d9bde49b96de7
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-06-09 16:37:32 +0530

    Add files via upload

---
+++ findGreatestnumber.java
@@ -0,0 +1,24 @@
+package target200qQQQTTOOnnSS;
+
+import java.util.Scanner;
+
+public class findGreatestnumber 
+{
+	public static void main(String []args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number");
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		
+		if(a>b)
+		{
+			System.out.print(a);
+		}
+		else 
+		{
+		   System.out.println(b);
+		}
+	}
+
+}
