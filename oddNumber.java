307d9db43a73b220faa19b918ca3818196c818c8
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-06-13 21:58:33 +0530

    Add files via upload

---
+++ oddNumber.java
@@ -0,0 +1,17 @@
+package target200qQQQTTOOnnSS;
+
+public class oddNumber 
+{
+	public static void main(String []args)
+	{
+		int number=100;
+		for(int i=0; i<=number; i++)
+		{
+			if(i%2!=0)
+			{
+				System.out.println(i);
+			}
+		}
+	}
+
+}
