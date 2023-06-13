bb69bba658d582945fd36dd8ed0d940dc8750d23
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-06-13 21:35:36 +0530

    Add files via upload

---
+++ evenNumber.java
@@ -0,0 +1,17 @@
+package target200qQQQTTOOnnSS;
+
+public class evenNumber 
+{
+	public static void main(String []args)
+	{
+		int number=100;
+		for(int i=0; i<=100; i++)
+		{
+			if(i%2==0)
+			{
+				System.out.println(i);
+			}
+		}
+	}
+
+}
