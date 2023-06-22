e73938c8cf3de792b02a0092e86778258465ba56
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-06-22 22:07:36 +0530

    Add files via upload

--- oddNumber.java
+++ oddNumber.java
@@ -1,17 +1,19 @@
-package target200qQQQTTOOnnSS;
+package NumberProgram;
 
 public class oddNumber 
 {
-	public static void main(String []args)
+	public static void main(String[] args) 
 	{
-		int number=100;
-		for(int i=0; i<=number; i++)
+		int[] arr=new int[] {1,2,3,4,5,6,7,8,9};
+		System.out.println(" Enter the number");
+		for(int i=0; i<=arr.length; i++)
 		{
-			if(i%2!=0)
+			if(arr[i]%2!=0)
 			{
-				System.out.println(i);
+				System.out.println("odd number"  +arr[i]);
 			}
 		}
+		
 	}
 
 }
