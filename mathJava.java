a35f8a9f076b0282c48976f6d8a9cd40cedd7570
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-06-23 19:50:46 +0530

    Add files via upload

---
+++ mathJava.java
@@ -0,0 +1,14 @@
+package edu.selfpractice.programing.string;
+
+public class mathJava 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("math all formate");
+		System.out.println("max  "+Math.min(10, 55));
+		System.out.println("min   "+Math.min(55, 103));
+		System.out.println("squre  "+Math.sqrt(121));
+		
+	}
+
+}
